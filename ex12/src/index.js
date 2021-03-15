dadde1d2d5be8585c4df759f6cd1628c709122dc
salkorad <salko.radovovic@gmail.com>
2021-03-15 23:34:20 +0100

    Random Whole Numbers

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function randomInteger() {
+    return integer = Math.floor(Math.random() * 30);
+}
+randomInteger();
+console.log(integer);
+// Only change code above this line
+module.exports = randomInteger;
