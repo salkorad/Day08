215d580658bd1fe4d4eaa2db97107df50a9a1e91
salkorad <salko.radovovic@gmail.com>
2021-03-15 23:45:08 +0100

    Random Whole Numbers within a Range

---
+++ ex13/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+    return integer = Math.floor(Math.random() * (maxNumber - minNumber +1)) + minNumber;
+}
+randomRangeNumber(1, 7);
+console.log(integer);
+// Only change code above this line
+module.exports = randomRangeNumber;
