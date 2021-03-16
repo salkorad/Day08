b44ea22fb10108d031c89e802879265ffc21e7ff
salkorad <salko.radovovic@gmail.com>
2021-03-16 01:41:38 +0100

    Global vs Local Scope

---
+++ ex05/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+var dress = "little dress";
+
+function myDress() {
+    var dress = "ball gown";
+// only change code above this line
+
+    return dress;
+    
+}
+// only change code below this line
+
+myDress();
+
+// only change code above this line
+console.log(dress);
+
+module.exports = myDress;
